6be7f41eb4311a4953624c82f3118b605af591d4
efouillet <elena.fouillet@epita.fr>
2024-01-03 19:49:31 +0100

    create file for mappping

--- etl/cdm_measurement.R
+++ etl/cdm_measurement.R
@@ -9,6 +9,7 @@ library(dplyr)
 # Charger les fichiers de connexion
 source("etl/connect_broadsea.R")
 source("etl/mappage_id.R")
+source("etl/mapping_functions.R")
 
 # Connexion à la base de données de broadsea
 con <- connect_broadsea()
@@ -38,9 +39,9 @@ query_create_table <- paste0(
 dbClearResult(measurement)
 dbExecute(con, query_create_table)
 
-# Mapping pour concept_name
+# Mapping
 concept <- dbSendQuery(con, "SELECT * FROM demo_cdm.concept;")
-resconcept <- dbFetch(concept , -1)
+resconcept <- dbFetch(concept, -1)
 dbClearResult(concept)
 
 resconcept_filtered <- resconcept %>%
@@ -48,53 +49,8 @@ resconcept_filtered <- resconcept %>%
 
 mimic_info <- df_mimic_d_labitems[, c("itemid", "label")]
 
-# Initialisez un dictionnaire vide
-mimic_omop_mapping <- data.frame(
-  item_id_mimic = mimic_info$itemid,
-  label_mimic = mimic_info$label,
-  concept_name_omop = NA,
-  concept_code_omop = NA,
-  concept_id_omop = NA
-)
-
-# Correspondances concept_name et concept_code
-concept_mapping <- list(
-  "Hemoglobin" = c("718-7"),
-  "Glucose" = c("2345-7"),
-  "Calcium" = c("17861-6"),
-  "Temperature" = c("8331-1"),
-  "Alanine" = c("1742-6"),
-  "Platelet" = c("26515-7"),
-  "Thyroxine" = c("3024-7"),
-  "Hematocrit" = c("20570-8"),
-  "White Blood" = c("26464-8"),
-  "Urea nitrogen" = c("3094-0"),
-  "Aspartate aminotransferase" = c("1920-8"),
-  "Chloride" = c("2075-0"),
-  "Creatinine" = c("2075-0"),
-  "Sodium" = c("2951-2"),
-  "Protein" = c("2885-2"),
-  "Potassium" = c("2823-3"),
-  "Albumin" = c("1751-7"),
-  "Red blood" = c("26453-1"),
-  "Bilirubin" = c("42719-5"),
-  "Alkaline" = c("6768-6")
-)
-
-# Parcourir les correspondances et mettre à jour le mapping
-for (concept_name in names(concept_mapping)) {
-  matching_items <- grep(concept_name, mimic_info$label, ignore.case = TRUE, value = TRUE)
-  
-  if (length(matching_items) > 0) {
-    mimic_omop_mapping$concept_name_omop[mimic_omop_mapping$label_mimic %in% matching_items] <- concept_name
-    mimic_omop_mapping$concept_code_omop[mimic_omop_mapping$label_mimic %in% matching_items] <- concept_mapping[[concept_name]]
-    
-    # Chercher le concept_id correspondant dans resconcept_filtered
-    matching_concept_id <- resconcept_filtered$concept_id[resconcept_filtered$concept_code == concept_mapping[[concept_name]]]
-    
-    mimic_omop_mapping$concept_id_omop[mimic_omop_mapping$label_mimic %in% matching_items] <- matching_concept_id
-  }
-}
+# Utiliser la fonction de mapping
+mimic_omop_mapping <- measurement_concept_mapping(mimic_info, resconcept_filtered)
 
 # Affichage des résultats
 result <- df_mimic_labevents %>%
