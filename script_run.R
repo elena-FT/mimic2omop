25a02c8eebf020fb2fc3146f18e4f981863445ec
efouillet <elena.fouillet@epita.fr>
2024-01-05 11:20:56 +0100

    update script run

--- script_run.R
+++ script_run.R
@@ -1,3 +1,13 @@
-# Charger le fichier de connexion
+#script_run.R
+
 source("etl/usefull_fonctions.R")
 source("etl/cdm_person.R")
+source("etl/cdm_death.R")
+source("etl/cdm_drug_exposure.R")
+source("etl/cdm_measurement.R")
+source("etl/cdm_procedure_occurrence.R")
+source("etl/cdm_specimen.R")
+
+con <- connect_broadsea()
+dbListTables(con)
+dbDisconnect(con)
